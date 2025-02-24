23a6a6ef40872200020952a201abc4d407cf879e
theendofme <theendofme60@gmail.com>
2025-02-24 20:51:52 +0200

    Задания робота с операторами

---
+++ Student2/src/main/java/Opera/Operators.java
@@ -0,0 +1,40 @@
+package Opera;
+
+public class Operators {
+    public static void main(String[] args) {
+        // S = a * b, где a и b – длина и ширина прямоугольника соответственно.
+        // double a = 3 , b = 5, c =5 ;
+       // System.out.println("Площа прямогулоьныка: "+ ( a * b ));
+        // Середнє значення трьох чисел
+       // System.out.println("Середня значення трьох чисел: "+ (a + b + c)/3);
+        // Залишок від ділення двох чисел
+        //System.out.println( "Залишок від ділення двох чисел: "+(a/b));
+
+        //Створи програму, яка оголошує три булеві змінні
+                //boolean isAdmin = true;       // Чи є користувач адміністратором ;
+               // boolean hasPermission = false; // Чи має користувач необхідні права ;
+               // boolean isBanned =  false;    // Чи заблоковано користувача;
+               // boolean isAccessAllowed = isAdmin || (hasPermission && !isBanned) ;
+                // System.out.println("Доступ дозволено: " + isAccessAllowed) ;
+
+        //Напиши програму, яка порівнює два цілі числа за допомогою всіх операторів порівняння
+
+        //int a = (int) (Math.random() * 11); // Генерация числа от 0 до 10
+        //int b = (int) (Math.random() * 11); // Генерация числа от 0 до 10
+        //System.out.println("Рівність: " + (a==b));
+        //System.out.println("Нерівність: " + (a!=b));
+        //System.out.println("Більше: " + (a>b));
+        //System.out.println("Менше: " + (a<b));
+        //System.out.println("Більше або дорівнює: " + (a>=b));
+        //System.out.println("Менше або дорівнює: " + (a<=b));
+                String str_1 = "2";
+                String str_2 = "10";
+        System.out.println(str_1.equals(str_2));
+        System.out.println(str_1 == (str_2));
+
+
+    }
+}
+
+
+
