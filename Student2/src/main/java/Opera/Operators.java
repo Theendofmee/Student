404426ebaf473d35f114246b59fbc62219c63eb5
theendofme <theendofme60@gmail.com>
2025-02-26 19:25:32 +0200

    іауіаіуа

--- Student2/src/main/java/Opera/Operators.java
+++ Student2/src/main/java/Opera/Operators.java
@@ -2,40 +2,82 @@ package Opera;
 
 public class Operators {
     public static void main(String[] args) {
-        // S = a * b, где a и b – длина и ширина прямоугольника соответственно.
-        // double a = 3 , b = 5, c =5 ;
-       // System.out.println("Площа прямогулоьныка: "+ ( a * b ));
-        // Середнє значення трьох чисел
-       // System.out.println("Середня значення трьох чисел: "+ (a + b + c)/3);
-        // Залишок від ділення двох чисел
-        //System.out.println( "Залишок від ділення двох чисел: "+(a/b));
-
-        //Створи програму, яка оголошує три булеві змінні
-                //boolean isAdmin = true;       // Чи є користувач адміністратором ;
-               // boolean hasPermission = false; // Чи має користувач необхідні права ;
-               // boolean isBanned =  false;    // Чи заблоковано користувача;
-               // boolean isAccessAllowed = isAdmin || (hasPermission && !isBanned) ;
-                // System.out.println("Доступ дозволено: " + isAccessAllowed) ;
-
-        //Напиши програму, яка порівнює два цілі числа за допомогою всіх операторів порівняння
-
-        //int a = (int) (Math.random() * 11); // Генерация числа от 0 до 10
-        //int b = (int) (Math.random() * 11); // Генерация числа от 0 до 10
-        //System.out.println("Рівність: " + (a==b));
-        //System.out.println("Нерівність: " + (a!=b));
-        //System.out.println("Більше: " + (a>b));
-        //System.out.println("Менше: " + (a<b));
-        //System.out.println("Більше або дорівнює: " + (a>=b));
-        //System.out.println("Менше або дорівнює: " + (a<=b));
-
-        //Також порівняйте два рядки, демонструючи різницю між використанням оператора == та методу equals().
-        //String str_1 = "2";
-        //String str_2 = "2";
-        //System.out.println(str_1.equals(str_2));
-         //System.out.println(str_1 == (str_2));
+        /*//S = a * b.
+        double width = 3, length = 5, c = 5;
+        System.out.println("area: " + (width * length));
+        the average of three numbers
+        System.out.println("average : "+ (width + length + c)/3);
+        the remainder of the division of two numbers
+        System.out.println( "remainder: "+(width%length));*/
 
 
+        /*//Створіть програму, яка оголошує три логічні змінні.
+        boolean isAdmin = true;
+        boolean hasPermission = false;
+        boolean isBanned =  false;
+        boolean isAccessAllowed = isAdmin || (hasPermission && !isBanned) ;
+        System.out.println("Access granted: " + isAccessAllowed) ;*/
 
+
+        /*//Напиши програму, яка порівнює два цілі числа за допомогою всіх операторів порівняння.
+        int a = (int) (Math.random() * 11);
+        int b = (int) (Math.random() * 11);
+        System.out.println("Equality : " + (a == b));
+        System.out.println("Inequality: " + (a != b));
+        System.out.println("Greater: " + (a > b));
+        System.out.println("Less: " + (a < b));
+        System.out.println("Greater than or equal to: " + (a >= b));
+        System.out.println("Less than or equal to: " + (a <= b));*/
+
+
+        /*//Також порівняйте два рядки, демонструючи різницю між використанням оператора == та методу equals().
+        String str_1 = "2";
+        String str_2 = "2";
+        System.out.println(str_1.equals(str_2));
+        System.out.println(str_1 == (str_2));*/
+
+
+       /* //Напиши програму, яка виводить послідовність чисел від 1 до 5, використовуючи три різні цикли.
+       int number = 1;
+        for (; number < 6; number = number + 1) {
+            System.out.println(number);
+        }
+        while (number < 6) {
+            System.out.println(number);
+            number = number + 1;
+        }
+        do {
+            System.out.println(number);
+            number = number + 1;
+        } while (number < 6);*/
+
+
+        /*//За допомогою if...else класифікує введене число як позитивне, негативне або рівне нулю.
+        var number = 1;
+        if (number > 0) {
+            System.out.println("Number is Positive");
+        } else if (number == 0) {
+            System.out.println("Number is Zero");
+        } else {
+            System.out.println("Number is Negative");
+        }*/
+
+
+        /*//Напиши програму, яка має змінну choice, значення якої можна змінити у коді для симуляції різних варіантів вибору.
+        var choice = 4;
+        switch (choice) {
+            case 1:
+                System.out.println("Hello");
+                break;
+            case 2:
+                System.out.println("Short info about a program");
+                break;
+            case 3:
+                System.out.println("Exit");
+                break;
+            default:
+                System.out.println("Incorrect Choise");
+        }*/
     }
 }
 
