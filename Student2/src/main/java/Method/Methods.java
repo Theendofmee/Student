d34d14f03e64b8a137be23866d5106b5527b6dba
theendofme <theendofme60@gmail.com>
2025-03-01 15:06:17 +0200

    Methods

--- Student2/src/main/java/Method/Methods.java
+++ Student2/src/main/java/Method/Methods.java
@@ -74,7 +74,7 @@ public class Methods {
         if (n == 0 || n == 1) {
             return 1;
         }
-        return n * factorial(n - 1);
+        return n * factorial(n - 1);//
     }
 */
 }
